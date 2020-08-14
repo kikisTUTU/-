1d822a236526f48a8c95b5bb5fbf09e0711fb57c
图图 <tutu@tutudeMacBook-Pro.local>
2020-08-14 15:25:10 +0800

    m

---
+++ 滤镜分屏特效/滤镜分屏特效/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  滤镜分屏特效
+//
+//  Created by 图图 on 2020/8/11.
+//  Copyright © 2020 图图. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 滤镜分屏特效/滤镜分屏特效/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  滤镜分屏特效
+//
+//  Created by 图图 on 2020/8/11.
+//  Copyright © 2020 图图. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
